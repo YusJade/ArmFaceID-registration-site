543900cb788e743a41983dfc6e9f84827cd54da7
idealoong <335936213@qq.com>
2024-11-14 19:54:49 +0800

    表单验证修正；注册接口初版更新

--- src/views/test_register.vue
+++ src/views/test_register.vue
@@ -16,7 +16,7 @@
             <el-input v-model="ruleForm.name" required></el-input>
           </el-form-item>
           <el-form-item label="邮箱" prop="email">
-            <el-input v-model="name" required></el-input>
+            <el-input v-model="ruleForm.email" required></el-input>
           </el-form-item>
           <el-form-item label="上传图片">
             <el-upload v-model:file-list="fileList" class="upload-demo"
@@ -27,11 +27,8 @@
             </el-upload>
           </el-form-item>
           <el-form-item>
-            <el-button style="margin: 0 auto;" type="primary"
-                 native-type="submit">注册</el-button>
+            <el-button style="margin: 0 auto;" type="primary" @click="register">注册</el-button>
           </el-form-item>
-          <el-button style="margin: 0 auto;" type="success" @click="testRPC">点我发送一个 rpc
-            请求</el-button>
         </el-form>
         </div>
       </el-main>
@@ -52,7 +49,6 @@ import type { RpcError } from 'grpc-web'
 const fileList = ref<UploadUserFile[]>([
 ])
 
-const name = ref('');
 const imageBase64 = ref<string>();
 
 
@@ -84,7 +80,6 @@ const getUint8Array = (fileRaw: File) => {
   })
 }
 
-
 const getBase64 = (fileRaw: File) => {
   return new Promise(function (resolve, reject) {
     const reader = new FileReader()
@@ -102,25 +97,37 @@ const getBase64 = (fileRaw: File) => {
   })
 }
 
+const register = () => {
+  // 检查是否上传图片
+  if (fileList.value.length === 0) {
+    ElMessage.error('请上传一张图片')
+    return
+  }
 
-const testRPC = () => {
-  //FaceRpc.Recognize('')
-  const userInfo = new UserInfo();
-  userInfo.setEmail('test@email.com')
-  userInfo.setUserName('testPerson')
-  userInfo.setFaceImage(imageBase64.value as string)
-  // TODO: MsgBox
-  console.log(`准备上传图片 ${fileList.value[0]}`)
-  getUint8Array(fileList.value[0].raw as File).then((arr) => {
-    // 记得 return
-    return FaceRpc.Recognize(arr)
+  // 获取图片的 base64 编码
+  getBase64(fileList.value[0].raw as File).then((base64) => {
+    const userInfo = new UserInfo()
+
+    // 清理图片的 Base64 字符串（去掉头部部分）
+    base64 = base64.replace('data:image/png;base64,', '')
+
+    // 设置用户信息
+    userInfo.setFaceImage(base64 as string)
+    userInfo.setEmail(ruleForm.email)
+    userInfo.setUserName(ruleForm.name)
+
+    // 调用后端进行注册
+    console.log(`准备上传图片，姓名：${ruleForm.name}, 邮箱：${ruleForm.email}`)
+
+    // 通过 FaceRpc 注册用户
+    return FaceRpc.Register(userInfo)
   })
-    .then((resp: RecognitionResponse | null) => {
-      Message(`${resp?.getRes.name}，欢迎你`)
-    }, (err: RpcError) => {
-      console.log(err)
-      Message(`${err?.message}`)
-    })
+    .then((resp) => {
+      console.log(resp)
+    },
+      (err: RpcError) => {
+        Message(err.message)
+      })
 }
 
 const ruleFormRef = ref<FormInstance>()
@@ -130,14 +137,49 @@ const ruleForm = reactive({
   email: ''
 })
 
+// 名字校验函数
+const validateName = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    return callback(new Error('请输入名字'))
+  }
+
+  // 名字长度检查
+  if (value.length < 3 || value.length > 5) {
+    return callback(new Error('名字长度必须在3到5个字符之间'))
+  }
+
+  // 名字只能包含字母和空格
+  const nameRegex = /^[a-zA-Z\s]+$/
+  if (!nameRegex.test(value)) {
+    return callback(new Error('名字只能包含字母和空格'))
+  }
+
+  callback()
+}
+
+
+// 邮箱校验函数
+const validateEmail = (rule: any, value: any, callback: any) => {
+  if (!value) {
+    return callback(new Error('请输入邮箱'))
+  }
+
+  // 邮箱正则表达式（可以根据需要修改）
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+
+  if (!emailRegex.test(value)) {
+    callback(new Error('请检查邮箱格式'))
+  } else {
+    callback()
+  }
+}
+
 const rules = reactive<FormRules>({
   name: [
-    { required: true, message: '请输入姓名', trigger: 'blur' },
-    { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+    { required: true, validator: validateName, trigger: 'blur' }
   ],
   email: [
-    { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-    { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+    { required: true, validator: validateEmail, trigger: 'blur' }
   ]
 })
 
